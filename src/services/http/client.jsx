8ae9e43a5a1b081412a8ad3b7f62b532ea3126df
ROHIT MANNA <rohitmanna55@gmail.com>
2026-02-27 11:42:24 +0530

    Vercel fixes

--- src/services/http/client.jsx
+++ src/services/http/client.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:5000";
+
 export const http = axios.create({
-  baseURL: "http://127.0.0.1:5000",
+  baseURL,
   timeout: 30000,
 });
 
